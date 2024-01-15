3e595f66d1008a7ed597cec451bfa9cc3abf4982
GeorgeBatiashvili <g.batiashviliii@gmail.com>
2024-01-16 01:53:38 +0400

    script

---
+++ script.js
@@ -0,0 +1,48 @@
+let userAge = 100;
+
+if (userAge < 50) {
+    console.log("is less than 50");
+} 
+else if (userAge > 20) {
+ console.log("is more than 20");
+}
+else {
+    console.log("error");
+}
+
+
+
+
+
+let userName = "mariam"
+
+if (userName == "mariam") {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+
+
+
+
+let expretion = "mariam"
+
+switch(expression) {
+    case "mariam":
+        console.log("true");
+        break;
+case "!mariam":
+    console.log("false");
+}
+
+
+
+
+
+let userName = 
+
+"mariam" ? true : "false";
+
+console.log(userName);
